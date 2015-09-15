51446d0df8dc126562692bcb4add6e2558a5257b
juanvallejo <juan.vallejo.12@cnu.edu>
2015-09-15 15:22:22 -0400

    update syntax

--- scripts/udpsink.sh
+++ scripts/udpsink.sh
@@ -10,4 +10,4 @@ fi
 
 # pipes image from camera 0/1 to specified ip address using udpsink
 echo opening up pipe on host $ip with port 3000
-gst-launch-0.10 -v v4l2src device=/dev/video1 ! video/x-raw-yuv,width=320,height=240 ! queue ! ffmpegcolorspace ! jpegenc ! multipartmux! udpsink host= $ip port=3000 
+gst-launch-0.10 -v v4l2src device=/dev/video1 ! video/x-raw-yuv,width=320,height=240 ! queue ! ffmpegcolorspace ! jpegenc ! multipartmux! udpsink host=$ip port=3000 
